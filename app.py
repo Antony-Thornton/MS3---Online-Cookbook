0ef1828a417e9d88b6be74256a07756ab8b4a07a
Antony-Thornton <Antony_thornton_p@outlook.com>
2021-09-25 10:30:01 +0000

    added os code from CI video

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def hello():
     return "Hello World ... again!"
 
-
+    
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
