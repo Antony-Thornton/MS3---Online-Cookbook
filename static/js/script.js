e850dea03b88ac33c0fc7e107f73e9cc99b3b54c
Antony-Thornton <Antony_thornton_p@outlook.com>
2022-02-12 19:02:36 +0000

    Added content to profile page.
    Added materialize boxes to the profile page. Need to add code to app.py to pull user email.

--- static/js/script.js
+++ static/js/script.js
@@ -5,3 +5,16 @@
     $('.sidenav').sidenav({edge: "right"});
   });
 
+
+
+/* Javascript for collapsible section in profile html page - https://materializecss.com/collapsible.html*/
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
