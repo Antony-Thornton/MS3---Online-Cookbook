291a4fe065ad0c8d474794679a0b06c883f8f741
Antony-Thornton <Antony_thornton_p@outlook.com>
2022-03-12 11:38:27 +0000

    Added comments to javascript file

--- static/js/script.js
+++ static/js/script.js
@@ -4,30 +4,24 @@
   $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
   });
+/* End */
 
 
-
-/* Javascript for collapsible section in profile html page - https://materializecss.com/collapsible.html*/
+/* Javascript for collapsible section in profile html page - https://materializecss.com/collapsible.html */
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.collapsible');
   var instances = M.Collapsible.init(elems, options);
 });
+/* End */
 
-// Or with jQuery
-
-$(document).ready(function(){
-  $('.collapsible').collapsible();
-});
-
-
-
-
+/* Toggle buttons for managing recipes */
+/* https://stackoverflow.com/questions/30523370/javascript-expand-collapse-button */
+/* Delete */
 $('.button_delete').click(function(){
   $('.wrapper_delete').toggle();
 });  
-
-
-
+/* edit */
 $('.button_edit').click(function(){
   $('.wrapper_edit').toggle();
 });  
+/* End toggle buttons */
